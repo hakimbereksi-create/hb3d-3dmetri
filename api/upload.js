7037944d3dac338d27893280d8c1f3dd48cc82ed
hakimbereksi-create <hakimbereksi@gmail.com>
2026-03-14 09:50:55 +0100

    Create upload.js

---
+++ api/upload.js
@@ -0,0 +1,4 @@
+// Copie code AWS S3 du message précédent
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+// ... (exactement le même)
